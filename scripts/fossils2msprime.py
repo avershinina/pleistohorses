c368719faf3c3dcf65cca84b8a66cc7cc2b7aed4
avershinina <vershininaalissa@gmail.com>
2017-12-15 14:52:12 -0800

    Create fossils2msprime.py

---
+++ scripts/fossils2msprime.py
@@ -0,0 +1,31 @@
+# python3
+# 15 Dec 2017
+# A. Vershinina
+# Goal: generate msprime "samples" config
+# imitating a number of ancient samples 
+# having different radiocarbon ages.
+
+def generate_years(youngest_date, oldest_date, nsamples):
+    """
+    generate a list of dates - imitate a number (=nsamples) of paleo samples
+    """
+    years = random.sample(range(youngest_date, oldest_date), nsamples)
+    return years
+    
+def years2samples(years, gen_time, pop_id):
+    """
+    generate msprime sample list 
+    convert time points to generations,
+    pop_id - for which population to generate config parameters
+    """
+    generations = list(map(lambda x: x/gen_time, years))
+    N = [int(pop_id)] * len(generations)
+    return zip(N, generations)
+    
+y = generate_years(10,100, 5)
+fossils = years2samples(y, 5, 0)
+for pop_id, t in fossils:
+    # msprime generates haploids, so we need 2 haplotypes for each time point
+    print (
+        "[msprime.Sample(population={}, time={}) for _ in range(2)] +".format(pop_id, t), 
+        file = open("msprime_samples.txt", "a"))
