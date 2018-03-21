6f58ec5196dd42ef92347f1a689f645af698e4c9
avershinina <vershininaalissa@gmail.com>
2018-03-21 14:56:42 -0700

    Update antcaller_reheader_bgzip_tabix.sh

--- variant_calling/antcaller_reheader_bgzip_tabix.sh
+++ variant_calling/antcaller_reheader_bgzip_tabix.sh
@@ -1,13 +1,12 @@
 #!/usr/local/bin/bash
-# 21 Feb 2018
+# 21 March 2018
 # A. Vershinina
-# Goal: AntCaller outputs vcf file without header (i. e. the file has only variants). To process this file we need vcf formatting with the corresponding info.
-# Create a file vcf_header.txt. This can contain vcf header created by another program from the same BAM file, such as mpileup. Or any other header of your choice. GZIP the header file!
+# Goal: AntCaller outputs vcf file without header (variants only). To process this file we need vcf formatting with info.
+# Create a file vcf_header.txt. This should contain vcf header created by another program from the same BAM file, such as mpileup.  GZIP header!
 # Example:
 # head -n 40 samtoolsgenerated.vcf > vcf_header.txt
 # sed -i '/INDEL/d' vcf_header.txt
 # sed -i '/samtool/d' vcf_header.txt
-# gzip vcf_header.txt
 
 if [ "$#" -lt 1 ] # are there less than 1 argument?
 then
@@ -18,6 +17,7 @@ fi
 
 FILES=*.ant.vcf.gz
 FIXVCF='java -jar /soe/avershinina/tools/jvarkit/dist/fixvcf.jar'
+PICARD='java -jar /soe/avershinina/tools/picard.jar'
 
 for f in $FILES
 do
@@ -25,13 +25,22 @@ do
 	wait
 	zcat $1 $f > tmp && mv tmp ${NAME}.tmp.vcf
 	wait
-	$FIXVCF < ${NAME}.tmp.vcf | vcf-sort | bgzip > ${NAME}.ant.h.vcf.bgz
+	${FIXVCF} < ${NAME}.tmp.vcf > ${NAME}.tmp2.vcf 
+	echo "$f FixVCF done"
+	# | vcf-sort | bgzip > ${NAME}.ant.h.vcf.bgz
 	wait
-	rm -f ${NAME}.tmp.vcf
+	${PICARD} SortVcf I=${NAME}.tmp2.vcf O=${NAME}.ant.h.vcf
+	echo "$f PICARD SortVcf done"
 	wait
+	bgzip --stdout ${NAME}.ant.h.vcf > ${NAME}.ant.h.vcf.bgz
 	tabix -p vcf ${NAME}.ant.h.vcf.bgz
+	echo "$f bgzip, tabix done"
 	wait
-	echo "Sample $f done"
+	rm -f ${NAME}.ant.h.vcf
+	rm -f ${NAME}.tmp2.vcf
+	rm -f ${NAME}.tmp.vcf
+	echo "processing for Sample $f done"
 done
 wait
-echo "Reheader, bgzip and tabix done"
+echo "Reheader, sorting, bgzip, and tabix done"
+		
