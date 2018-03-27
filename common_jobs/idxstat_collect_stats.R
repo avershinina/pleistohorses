071f7a59b8195f53813cc60fe6a8f0473d856ef6
avershinina <vershininaalissa@gmail.com>
2018-03-27 14:32:19 -0700

    Create idxstat_collect_stats.R

---
+++ common_jobs/idxstat_collect_stats.R
@@ -0,0 +1,32 @@
+# A. Vershinina
+# 27 March 2018
+# Goal: collect statistics from BWA indexstat output. 
+# This version is collecting only a number of mapped reads on chr 1,2 and 3. 
+# Input: folder with idxstat results
+# Output: summary csv file foldername.chr1-2-3-idxstats.csv
+
+args = commandArgs(trailingOnly=TRUE)
+filepathidx=args[1]
+
+filesidx <- list.files(path=filepathidx, pattern="*.rmdup.idxstats*", full.names=T, recursive=FALSE)
+datalist=list()
+#go through each sample
+i = 0
+for (f in filesidx)
+{
+  i = i + 1
+  idx <- read.csv(f, sep = '\t', header = F)
+  idx <- idx[(idx$V1=="chr1" | idx$V1=="chr2" | idx$V1=="chr3"),] # Change here to get other chromosomes 
+  samplename <- strsplit(f, '[/]')[[1]]
+  l <- length(samplename)
+  samplen <- strsplit(samplename, '[.]')[[l]][1]
+  idx[5] <- samplen
+  names(idx) <- c("chr", "ref_len", "mapped_reads", "unmapped_reads", "sample")
+  datalist[[i]] <- idx
+}
+
+output <- do.call(rbind, datalist)
+wd <- basename(getwd())
+write.csv(output, file=paste(wd, "chr1-2-3-idxstats.csv", sep="."))
+#write.table(utms, file=paste(x, ".mean", sep=""))
+cat("done\n")
