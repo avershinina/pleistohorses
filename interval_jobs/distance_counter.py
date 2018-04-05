2708ff456a046701b4590b6e9a9f3af33eaf250e
avershinina <vershininaalissa@gmail.com>
2018-04-04 20:40:26 -0700

    Create distance_counter.py

---
+++ interval_jobs/distance_counter.py
@@ -0,0 +1,32 @@
+# !/usr/bin/python3
+# 4 April 2018
+# Goal: calculate distances between features in bed file
+# Input: regular bed file parsable by pybedtools
+# Output: file with distance between 
+
+from pybedtools import BedTool
+inserts = BedTool('aGenomes.inserts.bed')
+delets = BedTool('aGenomes.delets.bed')
+dists_ins = []
+
+def subtract(bedfile):
+	dists=[]
+	features = (bedfile).count()
+	counter = features-1
+	for i in range (counter):
+		dists.append(bedfile[i+1].start-bedfile[i].end+1)
+	return dists
+
+d_i = subtract(inserts)
+d_d = subtract(delets)
+
+file_insert = open('insert_dists.txt', 'w')
+file_delet = open('delet_dists.txt', 'w')
+
+for item in d_i:
+	file_insert.write("%s\n" % item)
+file_insert.close()
+
+for item in d_d:
+	file_delet.write("%s\n" % item)
+file_delet.close()
