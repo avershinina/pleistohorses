c891848f088893eb58e3f5cabb2b0feacfb3d188
avershinina <vershininaalissa@gmail.com>
2018-09-14 15:50:22 -0700

    save to file

--- variant_calling/draw_features_spaced_by_n.py
+++ variant_calling/draw_features_spaced_by_n.py
@@ -4,14 +4,20 @@
 # Goal: take a BED file, containing snps.
 # Draw from the file only those snps, that are spaced by more than N bp between each other.
 # At! This now works regardless of chromosome number, so BED file should contain only elements of one chromosome.
+f='path/test_2.bed'
 import pandas as pd
-f='file.bed'
-snps2 = pd.read_csv(f, delimiter='\t', names = ["chr", "snp"])
-# chrs=snps2.chr.unique()
+snps = pd.read_csv(f, delimiter='\t', names =  ["chr", "snp"])
 prev = 0
-for i, item in enumerate(snps2['snp']):
+df = pd.DataFrame(columns=["chr", "snp"]) # new df to save snips
+a_list=[]
+for i, item in enumerate(snps['snp']):
     value = int(item)
     l = value - prev
-    if l > N: # change N to desired number of bp
+    if l > N: # change to desired number
+        a_list.append(value)
         prev = value
-        print(i,l) # TODO extract snps by index i
+        print(i,l)
+        print(a_list)
+df = pd.DataFrame({'chr': snps.chr[1], 'snp': a_list})
+del a_list
+df.to_csv("path/filtered.bed", encoding='utf-8', index=False, header=False, sep='\t')
