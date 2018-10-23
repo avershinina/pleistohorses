f4cfa46d6b7ca7205a6ac2595d3c2babb61e3379
avershinina <vershininaalissa@gmail.com>
2018-10-23 16:43:10 -0700

    Create bam2fasta.sh

---
+++ common_jobs/bam2fasta.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/bash
+# 23 Oct 2018
+# A. Vershinina
+# Goal: convert BAM files to FASTA files using ANGSD. Create two sets of fastas: with thransitions and without transitions.
+
+HOME=/avershinina # EDIT
+FILES=${HOME}/horse_genomes/*.bam
+ANGSD=${HOME}/tools/angsd/angsd
+FILTERS='-minQ 20 -minMapQ 30 -uniqueOnly -setMinDepth 5 -setMaxDepth 100 -iupacRatio 0.35'
+REF=${HOME}/horse_genomes/Equus_cab_nucl_wChrUn.fasta
+
+echo "Starting conversion, transitions included"
+
+for SAMPLE in ${FILES}; do
+	NAME=$(echo ${SAMPLE} | sed 's/[.].*//')
+	echo "Making w_Ti fasta for ${NAME}"
+	$ANGSD -doFasta 4 -doCounts 1 $FILTERS -i $SAMPLE -out ${NAME}_w_Ti
+done
+echo "With Ti conversion done"
+
+echo "Starting conversion, transitions excluded"
+
+for SAMPLE in ${FILES}; do
+	NAME=$(echo ${f} | sed 's/[.].*//')
+	echo "Making wo_Ti fasta for ${NAME}"
+	$ANGSD -doFasta 4 -doCounts 1 $FILTERS -rmTrans 1 -ref $REF -i $SAMPLE -out ${NAME}_wo_Ti
+done
+echo "Without Ti conversion done"
+
+
+mkdir ${HOME}/horse_genomes/fastas_w_Ti
+mkdir ${HOME}/horse_genomes/fastas_wo_Ti
+OUT_w_Ti=${HOME}/horse_genomes/fastas_w_Ti
+OUT_wo_Ti=${HOME}/horse_genomes/fastas_wo_Ti
+
+mv *_w_Ti $OUT_w_Ti
+mv *_wo_Ti $OUT_wo_Ti
+
