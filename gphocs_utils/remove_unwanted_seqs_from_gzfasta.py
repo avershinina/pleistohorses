d996ef0cf37c6356999f880dbc80ecfdb91ffcf0
avershinina <vershininaalissa@gmail.com>
2019-08-29 13:09:40 -0700

    Create remove_unwanted_seqs_from_gzfasta.py

---
+++ gphocs_utils/remove_unwanted_seqs_from_gzfasta.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# https://www.biostars.org/p/4881/
+
+import sys
+import gzip
+from Bio import SeqIO
+
+fasta_file = sys.argv[1]  # Input fasta file
+remove_file = sys.argv[2] # Input wanted file, one gene name per line
+result_file = sys.argv[3] # Output fasta file
+
+remove = set()
+with open(remove_file) as f:
+    for line in f:
+        line = line.strip()
+        if line != "":
+            remove.add(line)
+
+# fasta_sequences = SeqIO.parse(open(fasta_file),'fasta')
+
+with open(result_file, "w") as f, gzip.open(fasta_file, "rt") as handle:
+    for seq in SeqIO.parse(handle, "fasta"):
+    	nam = seq.id
+    	nuc = str(seq.seq)
+    	if nam not in remove and len(nuc) > 0:
+    		SeqIO.write([seq], f, "fasta")
